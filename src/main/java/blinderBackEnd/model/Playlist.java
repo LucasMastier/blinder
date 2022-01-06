7efac53e0a69b88dd5d1cedbefef75122cd1132f
Lucas Mastier <73110352+LucasMastier@users.noreply.github.com>
2022-01-06 21:33:10 +0100

    Added constructs for playlist

--- src/main/java/blinderBackEnd/model/Playlist.java
+++ src/main/java/blinderBackEnd/model/Playlist.java
@@ -4,8 +4,18 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Playlist {
+    String titre;
     List<Song> playlist = new ArrayList<>();
 
+
+    public Playlist(String titre, List<Song> playlist){
+        this.titre = titre;
+        this.playlist = playlist;
+    }
+
+    public Playlist(String titre){
+        this.titre = titre;
+    }
 
     public void addToPlaylist(Song song){
         playlist.add(song);
