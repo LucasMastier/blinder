e38ec39faa0aaa6d24c36208684b16fe31712e77
Lucas Mastier <73110352+LucasMastier@users.noreply.github.com>
2022-01-06 15:31:01 +0100

    Client/Server in progress

---
+++ src/main/java/blinderBackEnd/Server/ClientHandler.java
@@ -0,0 +1,37 @@
+package blinderBackEnd.Server;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+public class ClientHandler implements Runnable {
+    private Socket client;
+    private BufferedReader in;
+    private PrintWriter out;
+
+    public ClientHandler(Socket clientSocket) throws IOException {
+        this.client = clientSocket;
+        in = new BufferedReader(new InputStreamReader(client.getInputStream()));
+        out = new PrintWriter(client.getOutputStream(),true);
+    }
+
+    @Override
+    public void run() {
+        try{
+            while(true){
+
+            }
+        } finally {
+            out.close();
+
+            try {
+                in.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+        }
+    }
+}
--- src/main/java/blinderBackEnd/Server/Server.java
+++ src/main/java/blinderBackEnd/Server/Server.java
@@ -6,24 +6,30 @@ import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.ArrayList;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class Server {
     public static int PORT = 1236;
+    private static ArrayList<ClientHandler> clients = new ArrayList<>();
+    private static ExecutorService pool = Executors.newFixedThreadPool(4);
+
     public static void main(String[] args) throws IOException {
 
         ServerSocket listener = new ServerSocket(PORT);
 
-        System.out.println("[Server] Waiting client connection");
-        Socket client = listener.accept();
-        System.out.println("[Server] Connected to client!");
+        while(true){
+            System.out.println("[Server] Waiting client connection");
+            Socket client = listener.accept();
+            System.out.println("[Server] Connected to client!");
+            ClientHandler clientThread = new ClientHandler(client);
+            clients.add(clientThread);
 
-        PrintWriter out = new PrintWriter(client.getOutputStream(),true);
-        BufferedReader in = new BufferedReader(new InputStreamReader(client.getInputStream()));
-/*
-        try{
-            while(true){
+            pool.execute(clientThread);
+        }
 
-            }
-        }*/
+
+
     }
 }
