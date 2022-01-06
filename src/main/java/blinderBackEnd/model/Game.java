c8ea7e77f8ade5dcf9282af5300aa3167c1ab84e
84z2h <90423456+84z2h@users.noreply.github.com>
2022-01-06 21:49:02 +0100

    add player/game

---
+++ src/main/java/blinderBackEnd/model/Game.java
@@ -0,0 +1,32 @@
+package blinderBackEnd.model;
+import java.util.ArrayList;
+
+public class Game {
+    private ArrayList<Player> list_player;
+    private Playlist pl;
+
+    public Game(Playlist playlist){
+        list_player= new ArrayList<Player>();
+        pl= playlist;
+    }
+    public void addPlayer(Player j){
+        list_player.add(j);
+    }
+    public void removePlayer(Player j){
+        list_player.remove(j);
+    }
+    public String toString(){
+        String res="";
+        for (Player p: list_player) {
+            res = p.toString()+"\n";
+        }
+        return res;
+    }
+    public void scoreUpdate(String username){
+        for (Player p: list_player) {
+            if(username.equals(p.getUsername())) {
+                p.setScore(p.getScore()+1);
+            }
+        }
+    }
+}
