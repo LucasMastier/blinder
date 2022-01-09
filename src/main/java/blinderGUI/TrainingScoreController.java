ba7777c159382d6272d7ab2b74aed4564272c6ee
Lucas Mastier <73110352+LucasMastier@users.noreply.github.com>
2022-01-09 22:19:56 +0100

    Added replay training mode feature and back to main menu

--- src/main/java/blinderGUI/TrainingScoreController.java
+++ src/main/java/blinderGUI/TrainingScoreController.java
@@ -1,8 +1,13 @@
 package blinderGUI;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+
+import java.io.IOException;
+
+import static blinderGUI.Main.switchTo;
 
 public class TrainingScoreController {
     @FXML
@@ -11,7 +16,20 @@ public class TrainingScoreController {
     @FXML
     private Label scoreDisplay;
 
+    @FXML
+    private Button mainMenuButton;
+
     public void initialize(){
         //scoreDisplay.setText(phrase a inserer);
     }
+
+    public void switchToMainMenu(ActionEvent event) throws IOException {
+        switchTo(event, "MainMenu.fxml");
+    }
+
+    public void replayTrainingMode(ActionEvent event) throws IOException {
+        //Reset le compteur de score
+        switchTo(event, "TrainingGame.fxml");
+    }
+
 }
