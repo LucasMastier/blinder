7412d5b87f6f5f063148cc7cae1e83d7078080be
Lucas Mastier <73110352+LucasMastier@users.noreply.github.com>
2022-01-09 14:48:02 +0100

    Added Spotify Training mode page

--- src/main/java/blinderGUI/MainMenuController.java
+++ src/main/java/blinderGUI/MainMenuController.java
@@ -75,4 +75,13 @@ public class MainMenuController {
 
     }
 
+    @FXML
+    public void switchToSpotifyTraningMode(ActionEvent event) throws IOException {
+        FXMLLoader fxmlLoader = switchTo(event, "SpotifyTrainingMode.fxml");
+
+
+    }
+
+
+
 }
